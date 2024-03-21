c1f7708f0105754c4330328f2875f19c8c26d5d0
Sina Jangjoo <Sina Jangjoo@DESKTOP-LNFK5GK>
2024-03-21 04:53:09 +0330

    Final Commit

--- RedMango_API/Migrations/20240320033954_FinalMigration.cs
+++ RedMango_API/Migrations/20240320033954_FinalMigration.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace RedMango_API.Migrations
 {
     /// <inheritdoc />
-    public partial class ChangeBothTypeofDtoAndModelFieldImage : Migration
+    public partial class FinalMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
